ea866a3e8d0d8701f25c68dbeb56078672efeade
Lksfnd <fendlukas@pm.me>
2019-06-04 17:25:48 +0200

    Added travis & coveralls

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+    // Since the config is in /config, set root to the above directory
+    rootDir: '../',
     testEnvironment: 'node',
     transform: {
         // transform typescript files to js before running the tests
